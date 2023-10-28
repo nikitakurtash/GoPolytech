b623904e22a50ef1871f0a77adef35b71f6d1b5b
nikitakurtash <nikitakurtash@gmail.com>
2023-10-29 01:18:09 +0300

    add shellsort_Lab_2

--- Lab_2/file.go
+++ Lab_2/file.go
@@ -1,12 +1,23 @@
 package main
 
-func findKthLargest(nums []int, k int) int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] > nums[j] {
-				nums[i], nums[j] = nums[j], nums[i]
+func shellSort(nums []int) {
+	n := len(nums)
+	h := 1
+	for h < n/3 {
+		h = 3*h + 1
+	}
+
+	for h >= 1 {
+		for i := h; i < n; i++ {
+			for j := i; j >= h && nums[j] < nums[j-h]; j -= h {
+				nums[j], nums[j-h] = nums[j-h], nums[j]
 			}
 		}
+		h /= 3
 	}
+}
+
+func findKthLargest(nums []int, k int) int {
+	shellSort(nums)
 	return nums[len(nums)-k]
 }
